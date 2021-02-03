bd3a4a1c2061d2615efbfc5f1e4583f316cfbeba
IT99\install <test@test>
2021-02-03 16:33:54 +0200

    commit

---
+++ c#/01 ifElseSwitch/01 buyAnElephant/ByAnElephant/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ByAnElephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string choice;
+
+            Console.WriteLine("Hello! What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Well hello, "+name);
+            Console.WriteLine("Wanna buy some elephant?");
+            choice = Console.ReadLine();
+            if (choice == "Yes" || choice == "yes")
+            {
+                Console.WriteLine("Relax man, just a joke");
+            }else if (choice == "No" || choice == "no")
+            {
+                Console.WriteLine($"Everyone says \"{choice}\", but you buy");
+            }
+        }
+    }
+}
